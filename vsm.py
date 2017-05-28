32b3a852b063c94cb82c30bcad07fe313272df40
wilson8507 <wilson8507@gmail.com>
2017-05-28 15:29:55 +0800

    Add Okapi tf weighting && clean useless codes

--- vsm.py
+++ vsm.py
@@ -11,63 +11,48 @@ import math
 
 from Preprocessor import Preprocessor
 
-def build_trains_question_tfidf(dataframe):
-    question_list = dataframe["question1"].tolist() + dataframe["question2"].tolist()
-    print("question_list_length:", len(question_list))
-    vectorizer = CountVectorizer()
-    word_count_matrix = vectorizer.fit_transform(question_list)
+def build_question_tfidf_okapi(question1, question2):
+    doclen = [len(question1),  len(question2)]
+    avgdoclen = sum(doclen)/len(doclen)
+    vectorizer = CountVectorizer(preprocessor = Preprocessor)
+    word_count_matrix = vectorizer.fit_transform([question1, question2])
     #print(word_count_matrix._shape)
-    ''' use for compute idf dictionary
     word = vectorizer.get_feature_names()
     bool_word_count_matrix = word_count_matrix.astype(bool).astype(int)
     col_sum_matrix = bool_word_count_matrix.sum(axis = 0)
     #print(type(col_sum_matrix), col_sum_matrix, col_sum_matrix.item(0))
-    idf = defaultdict(int)
+    idf = defaultdict(lambda:1)
     total_doc = word_count_matrix.shape[0]
     for term_i in range(col_sum_matrix.shape[1]):
-        idf[word[term_i]] = math.log10(total_doc/col_sum_matrix.item(term_i))
+        idf[word[term_i]] = math.log10(total_doc/col_sum_matrix.item(term_i)) + 1
         #print(word[term_i],":", col_sum_matrix.item(term_i))
 
-    #print(word)
+    tfidf_matrix = []
+    for doc_index, doc_vector in enumerate( word_count_matrix.toarray()):
+        tfidf_vector = []
+        #print("doc_index:", doc_index,"doc_vector:",  doc_vector)
+        for index, tf in enumerate(doc_vector):
+            #print(index, tf)
+            tf_okapi = tf / (tf + 0.5 + 1.5 * doclen[doc_index] / avgdoclen)
+            #print(word[index], idf[word[index]])
+            tfidf_vector.append(tf_okapi*idf[word[index]])
+        tfidf_matrix.append(np.array(tfidf_vector).reshape(1, -1))
+
     #print(word_count_matrix.toarray())
-    '''
 
-    transformer = TfidfTransformer()
-    tfidf = transformer.fit_transform(word_count_matrix)
+    return tfidf_matrix
 
+
+def build_question_tfidf(question1, question2):
+    tfidf_vectorizer = TfidfVectorizer(preprocessor = Preprocessor)
+    tfidf = tfidf_vectorizer.fit_transform([question1, question2])
     return tfidf
-
-
-def build_test_question_tfidf(question1, question2, corpus_word = None, idf = None):
-    if corpus_word is None and idf is None:
-        tfidf_vectorizer = TfidfVectorizer(preprocessor = Preprocessor)
-        tfidf = tfidf_vectorizer.fit_transform([question1, question2])
-        return tfidf
-    if question1 is not "" and question2 is not "":
-        test_question_list = [question1 , question2]
-        tfidf = []
-        for question in test_question_list:
-            vectorizer = CountVectorizer()
-            question_word_count_matrix = vectorizer.fit_transform([question]).toarray()
-            word = vectorizer.get_feature_names()
-            index = { value:i for i, value in enumerate(word)}
-            question_tfidf = []
-            for term_i in range(len(corpus_word)):
-                if corpus_word[term_i] in set(word):
-                    question_tfidf.append(idf[corpus_word[term_i]]*question_word_count_matrix[0][index[corpus_word[term_i]]])
-                else:
-                    question_tfidf.append(0)
-            tfidf.append(question_tfidf)
-        return tfidf
-    else:
-        return [[1], [1]]
 
 if __name__ == "__main__":
     df_train = pd.read_csv("./test.csv")
     print("test_shape:(ori)", df_train.shape)
     print( df_train.dtypes)
     df_train.fillna(value="empty", inplace = True) # fill all nan data
-    question_total = df_train.shape[0] # get length of row
     print("test_shape:(dropped)", df_train.shape)
     with open("test_out.csv", "wt") as fout:
         fout.write("test_id,is_duplicate\n")
@@ -77,26 +62,7 @@ if __name__ == "__main__":
                 continue
             print(index, row["question1"], row["question2"])
             #print(build_test_question_tfidf(row["question1"], row["question2"]).toarray())
-            vector_1, vector_2 = build_test_question_tfidf(row["question1"], row["question2"])
-            #print(str(index)+","+ str(cosine_similarity(vector_1, vector_2)))
+            #vector_1, vector_2 = build_question_tfidf(row["question1"], row["question2"]) # raw tf weighting
+            vector_1, vector_2 = build_question_tfidf_okapi(row["question1"] , row["question2"]) # okapi tf weighting
+            print(str(index)+","+ str(cosine_similarity(vector_1, vector_2)))
             fout.write(str(index)+","+ str(cosine_similarity(vector_1, vector_2)[0][0])+"\n")
-#    tfidf = build_trains_question_tfidf(df_train)
-#
-#    del df_train
-#    question1_tfidf = tfidf[:question_total]
-#    question2_tfidf = tfidf[question_total:]
-#    print(question1_tfidf.shape)
-#    print(question2_tfidf.T.shape)
-#    print(type(question1_tfidf))
-#    print(question1_tfidf.dtype)
-#    df_output = pd.DataFrame(columns = ["is_duplicate"], dtype=float)
-#    for row in range(question1_tfidf.shape[0]):
-#        df_output.loc[row] = [cosine_similarity(question1_tfidf[row], question2_tfidf[row])[0][0]]
-#        print(row, cosine_similarity(question1_tfidf[row], question2_tfidf[row])[0][0])
-#        #print(row, "1:", question1_tfidf[row])
-#        #print(row, "2:", question2_tfidf[row])
-#    print(df_output.head())
-#    df_output.to_csv("out.csv", encoding='utf-8')
-
-    #print(len(similarity))
-    #print(average)
